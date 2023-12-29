46124040c0edb91228014ab6c3ce0a5224709925
ryeact-dev <rmontoya.umtc@gmail.com>
2023-12-29 11:13:50 +0800

    Add server connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import expres from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = expres();
 
