1c2a6c942caf2121c1d1dfaa6980551107787999
ryeact-dev <rmontoya.umtc@gmail.com>
2023-12-29 14:31:51 +0800

    feat: add signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import bcryptjs from 'bcryptjs';
+
+import User from '../models/user.model.js';
+
+export async function signup(req, res) {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 // Routes
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
+// Database connection
 mongoose
   .connect(process.env.MONGODB)
   .then(() => {
@@ -17,7 +19,10 @@ mongoose
 
 const app = expres();
 
+app.use(expres.json());
+
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on port: 3000');
