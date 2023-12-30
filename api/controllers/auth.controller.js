bd6f0b02ed00f063942d16a5203a105755e9256a
ryeact-dev <rmontoya.umtc@gmail.com>
2023-12-30 19:42:22 +0800

    feat: add sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
+// Sign Up User
 export async function signup(req, res, next) {
   const { username, email, password } = req.body;
 
@@ -11,6 +14,27 @@ export async function signup(req, res, next) {
   try {
     await newUser.save();
     res.status(201).json({ message: 'User created successfully' });
+  } catch (err) {
+    next(err);
+  }
+}
+
+// Sign In User
+export async function signin(req, res, next) {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.cookie('token', token, { httpOnly: true }).status(200).json(rest);
   } catch (err) {
     next(err);
   }
