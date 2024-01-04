244a9af8b5ba20eb174df436ccfdc9485b19af84
ryeact-dev <rmontoya.umtc@gmail.com>
2024-01-04 23:38:32 +0800

    pre installation set up

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-import expres from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 // Routes
@@ -19,14 +20,21 @@ mongoose
     console.log(err);
   });
 
-const app = expres();
+const __dirname = path.resolve();
 
-app.use(expres.json());
+const app = express();
+
+app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 // Error handler
 app.use((err, req, res, next) => {
