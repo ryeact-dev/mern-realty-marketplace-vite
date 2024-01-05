de0c9f441e56ff33b6d6e94c863f6134f0956b35
ryeact-dev <rmontoya.umtc@gmail.com>
2024-01-05 23:02:28 +0800

    fix: update vercel json

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://mern-realty-marketplace-vite.vercel.app',
-        // target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
