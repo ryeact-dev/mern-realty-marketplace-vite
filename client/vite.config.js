6060bc94eeb37e28f2ea6ca523638ace510834ee
ryeact-dev <rmontoya.umtc@gmail.com>
2024-01-06 00:32:35 +0800

    remove proxy server

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,15 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://mern-realty-marketplace-vite.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://mern-realty-marketplace-vite.vercel.app',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx'],
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
