856598702b77ad82634b175704f1f93187ab6720
ryeact-dev <rmontoya.umtc@gmail.com>
2024-01-06 08:54:35 +0800

    testing update cors origin

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: '*',
+    origin: process.env.VERCEL_URL,
     methods: '*',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
