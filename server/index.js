b5899060a4553c5bf93c68b510d9e44503759bb8
ryeact-dev <rmontoya.umtc@gmail.com>
2024-01-06 08:58:05 +0800

    fix: try localhost cors

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.VERCEL_URL,
+    origin: process.env.LOCALHOST_URL,
     methods: '*',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
